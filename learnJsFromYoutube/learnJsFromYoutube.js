1e6179ea39c5dea5a879c6905cdee4a99ac083d3
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-04-02 21:20:06 +1000

    First commit of < 4-2>  <21:18>:  start learnJsFromYoutube.js

---
+++ learnJsFromYoutube/learnJsFromYoutube.js
@@ -0,0 +1,23 @@
+// https://www.youtube.com/watch?v=h33Srr5J9nY
+// JavaScript ES6 Arrow Functions Tutorial 
+// Web Dev Simplified
+// 9:03/ 9:31
+// 
+
+// create a class
+// example =>
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+const person = new Person("Bob")
+console.log(person.name)
